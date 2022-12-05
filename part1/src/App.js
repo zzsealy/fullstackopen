e551e9e41a04a703af3481805462351301a04afc
zzsealy <1147575371@qq.com>
2022-12-05 21:34:10 +0800

    wip: 练习1.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,42 +1,53 @@
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
-const Hello = (props) => {
+const Header = (props) => {
+  return (
+    <div>{props.course}</div>
+  )
+}
+
+
+const Content = (props) => {
   return (
     <div>
-      <p>Hello {props.name}, your are {props.age} years old!</p>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
     </div>
+  )
+}
+
+
+const Total = (props) => {
+  return (
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   )
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 =  'State of a component'
+  const exercises3 = 14
+  
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name='DRQ' age={10 + 15}/>
-      <Hello name='Daisy' age={age} />
+      <Header course={course}/>
+      <Content part1={part1} part2={part2} part3={part3}
+        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
+
   )
 }
+
 
 export default App;
