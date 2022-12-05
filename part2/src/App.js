832c107cdd03161020326267ffe88f81f6ddfb4b
zzsealy <1147575371@qq.com>
2022-12-05 20:31:25 +0800

    wip: part1 初始化

---
+++ part2/src/App.js
@@ -0,0 +1,37 @@
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  const now = new Date();
+  const a = 10
+  const b = 20 
+  console.log('Hello from component')
+  return (
+  <div>
+      <p>Hello World, It is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a+b}
+      </p>
+  </div>
+  )
+}
+
+export default App;
