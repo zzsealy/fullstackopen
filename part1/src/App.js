f38c7f39569c8910dead0fbca6df19601c3e3cca
zzsealy <1147575371@qq.com>
2022-12-06 23:07:35 +0800

    wip: 调用函数重复渲染

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,27 +1,8 @@
-
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age  // 函数内定义函数
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old.
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-const App = () => {
-  const name = 'drq' 
-  const age = 25
-  
-  return(
-    <div>
-      <Hello name={name} age={age} />
-    </div >
-
-  )
-}
-
-
-export default App;
+export default App
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+let counter = 1
+
+const refresh = () => {
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <App counter={counter} />
+    )
+  }
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+setInterval(() => {
+    refresh()
+    counter += 1
+  }, 1000)
