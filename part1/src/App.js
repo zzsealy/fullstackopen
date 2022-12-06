e23d989a3d995fc0a85bfcfec424f5787a85b31e
zzsealy <1147575371@qq.com>
2022-12-06 22:22:42 +0800

    wip: 1.5 放在一个对象里面渲染

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,40 +3,43 @@ const Content = (props) => {
   const parts = props.parts
   return (
     <div>
-    <p>{parts[0].name} {parts[0].exercises}</p>
-    <p>{parts[1].name} {parts[1].exercises}</p>
-    <p>{parts[2].name} {parts[2].exercises}</p>
+      <p>{parts[0].name} {parts[0].exercises}</p>
+      <p>{parts[1].name} {parts[1].exercises}</p>
+      <p>{parts[2].name} {parts[2].exercises}</p>
     </div>
   )
 
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [{
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-    }]
+  const course = {
+    'name': 'Half Stack application development',
+    'parts': [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }]
+  }
   
   let count_exercises = 0
-  parts.forEach(part => {
-    count_exercises = count_exercises + part.exercises
-  })
+  course.parts.forEach(part => {
+        count_exercises = count_exercises + part.exercises
+      })
   
-  return (
+  return(
     <div>
-      <h1>{course}</h1>
-      <Content parts={ parts } />
+      <h1>{course.name}</h1>
+      <Content parts={ course.parts } />
       <strong><p>Number of exercises {count_exercises}</p></strong>
-    </div>
+    </div >
 
   )
 }
