f955de973252e8f54ddb6b65c9fdcc876225a41f
zzsealy <1147575371@qq.com>
2022-12-06 11:27:35 +0800

    wip: 练习1.4 jsx列表循环渲染

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,29 +1,34 @@
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [{
     name: 'Fundamentals of React',
     exercises: 10
-  }
-
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
-  }
+    }]
+  
+  const part_jsx = parts.map((part, index) => 
+    <p key={index}> {part.name} {part.exercises}</p>
+  )
+  let count_exercises = 0
+  parts.forEach(part => {
+    count_exercises = count_exercises + part.exercises
+  })
   
   return (
     <div>
       <h1>{course}</h1>
-      <p>{part1.name} {part1.exercises}</p>
-      <p>{part2.name} {part2.exercises}</p>
-      <p>{part3.name} {part3.exercises}</p>
-
-      <strong><p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p></strong>
+      <ul>
+        {part_jsx}
+      </ul>
+      <strong><p>Number of exercises {count_exercises}</p></strong>
     </div>
 
   )
