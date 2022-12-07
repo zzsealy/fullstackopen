b47c306eb0ecd49b6857f7cf535165096646f8d6
zzsealy <1147575371@qq.com>
2022-12-07 17:38:30 +0800

    wip: 点击记录 条件渲染

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,18 @@
 import {useState} from 'react'
 
+
+const History = (props) => {
+  if (props.clickHistory === 0) {
+    return (
+      <h1>你还没点击过左右按钮。</h1>
+    )
+  }
+
+  return (
+    <h1>你的点击记录是: {props.clickHistory}。</h1>
+  )
+}
+
 const App = () => {
   // react hooks
   const [clicks, setClicks] = useState({
@@ -7,16 +20,22 @@ const App = () => {
     'right': 0
   })
 
+  const [clickHistory, setClickHistory] = useState([])
+
   const leftClicks = () => {
     setClicks({...clicks, left: clicks.left + 1})
+    setClickHistory(clickHistory.concat('L'))
   }
 
   const rightClicks = () => {
     setClicks({...clicks, right: clicks.right + 1})
+    setClickHistory(clickHistory.concat('R'))
   }
+
 
   return (
     <div>
+      <History clickHistory={clickHistory} />
       <p>left click time: {clicks.left}</p>
       <p>right click time: {clicks.right}</p>
       <button onClick={leftClicks}>click left</button>
