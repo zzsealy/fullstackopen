c72cfa5ca5583f9d5f44e4e87a7c1fce687d5b09
zzsealy <1147575371@qq.com>
2022-12-07 17:26:13 +0800

    wip: 将两个状态更新在一个对象里

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,40 +1,26 @@
 import {useState} from 'react'
 
-const Display = (props) => <div>{ props.counter }</div>
-
-
-const Button = (props) => {
-  return (
-    <div>
-      <button onClick={props.onClick}>{props.text}</button>
-    </div>
-  )
-}
-
 const App = () => {
   // react hooks
-  const [counter, setCounter] = useState(0)
+  const [clicks, setClicks] = useState({
+    'left': 0,
+    'right': 0
+  })
 
-  const increaseByOne = () => {
-    setCounter(counter + 1)
-    console.log('clicked')
+  const leftClicks = () => {
+    setClicks({...clicks, left: clicks.left + 1})
   }
 
-  const decreaseByOne = () => {
-    setCounter(counter - 1)
+  const rightClicks = () => {
+    setClicks({...clicks, right: clicks.right + 1})
   }
-
-  const setZero = () => {
-    setCounter(0)
-  }
-
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="增加" />
-      <Button onClick={decreaseByOne} text="减少" />
-      <Button onClick={setZero} text="置0" />
+      <p>left click time: {clicks.left}</p>
+      <p>right click time: {clicks.right}</p>
+      <button onClick={leftClicks}>click left</button>
+      <button onClick={rightClicks}>click right</button>
     </div>
   )
 }
