fcbf06b71d9de7f81317f084e76f8df17fe8dbb6
zzsealy <1147575371@qq.com>
2022-12-08 17:49:16 +0800

    wip: practice2.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,53 @@
-import Note from './components/Note'
+const Header = (props) => {
+  return (
+    <h1>{props.name}</h1>
+  )
+}
 
 
-const App = (props) => {
-  // react hooks
-  const notes = props.notes
-  let contents = []
-  notes.forEach(note => {
-    contents = contents.concat(note.content) 
-  });
+const Part = ({ part }) => {
+  return (
+    <li>{part.name} {part.exercises}</li>
+  )
+}
 
+
+const Course = ({course}) => {
+  const {name, parts} = course
   return (
     <div>
+      <Header name={name} />
       <ul>
-        {notes.map(note => <Note key={note.id} note={note} /> 
-        )}
+        {parts.map(part => <Part key={part.id} part={part} />)} 
       </ul>
     </div>
   )
 }
 
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
-export default App;
+  return <Course course={course} />
+}
+
+export default App
