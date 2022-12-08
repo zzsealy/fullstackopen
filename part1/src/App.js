f59e8b79dab024c4ed5d17df7d6828be6a91e349
zzsealy <1147575371@qq.com>
2022-12-08 23:26:01 +0800

    wip: practice-2.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,51 +1,42 @@
 import { useState } from 'react'
-import Note from './components/Note'
+
+const Number = ({ person }) => {
+  return (
+    <li>{person.name}</li>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
 
 
-
-
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note......')
-  const [showAll, setShowAll] = useState(true)
-
-  const addNote = (event) => {
+  const submitNewName = (event) => {
     event.preventDefault()
-    console.log('button click :>> ', event.target);
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: true,
-      id: notes.length + 1
+    const newPerson = {
+      name: newName
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    setPersons(persons.concat(newPerson))
+    setNewName('')
   }
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value) 
-    setNewNote(event.target.value)  // event.target.value 指的是该元素的输入值
-  }
-
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll? "important": "All"}
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
-        <button type='submit'>save</button>
+      <h2>Phonebook</h2>
+      <form onSubmit={submitNewName}>
+        <input value={newName} onChange={handleNewName} />
+        <button type="submit">add</button>
       </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <Number key={person.name} person={person} />)}
+      </ul> 
     </div>
   )
 }
