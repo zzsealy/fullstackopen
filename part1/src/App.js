f4fdbb458679e4d5c64e2cb1ed08cd329c9433c0
zzsealy <1147575371@qq.com>
2022-12-09 17:14:56 +0800

    wip: practice-2.8 同时支持输入手机号

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,18 +2,23 @@ import { useState } from 'react'
 
 const Number = ({ person }) => {
   return (
-    <li>{person.name}</li>
+    <li>{person.name}: {person.phone}</li>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '88998899'}
   ])
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNewPhone = (event) => {
+    setNewPhone(event.target.value)
   }
 
 
@@ -22,10 +27,12 @@ const App = () => {
     let hasPerson = persons.filter((person) => person.name === newName)
     if (hasPerson.length === 0) {
       const newPerson = {
-        name: newName
+        name: newName,
+        phone: newPhone
       }
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewPhone('')
     } else {
       alert(`${newName} is already added to phone book`)
     }
@@ -34,14 +41,15 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={submitNewName}>
-        <input value={newName} onChange={handleNewName} />
-        <button type="submit">add</button>
-      </form>
-      <h2>Numbers</h2>
       <ul>
         {persons.map(person => <Number key={person.name} person={person} />)}
       </ul> 
+      <h2>Numbers</h2>
+      <form onSubmit={submitNewName}>
+        <input value={newName} placeholder='Please input a new name' onChange={handleNewName} />
+        <input value={newPhone} placeholder='Please input a new phone number' onChange={handleNewPhone} />
+        <button type="submit">add</button>
+      </form>
     </div>
   )
 }
