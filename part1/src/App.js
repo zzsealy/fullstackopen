8b8408cab37fec278318a8992fd72ce582b06494
zzsealy <1147575371@qq.com>
2022-12-21 16:45:14 +0800

    wip: parctice 2.12 从开放接口那数据 然后过滤渲染国家

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,36 +1,81 @@
 import { useState, useEffect} from 'react'
 import axios from 'axios'
 
-const OneNote = ({note}) => {
+const OneNote = ({country}) => {
   return (
     <li>
-      <p>{note.name}: {note.number}</p>
+      <p>{country.name.common}</p>
     </li>
   )
 }
 
-const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
-  const hooks = () => {
-    console.log('effect')
-    axios.get('http://localhost:3001/persons')
-      .then((response) => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }
-  useEffect(hooks, [])
-  console.log('render', notes.length, 'notes')
-
+const CountryDetail = ({ country }) => {
   return (
     <div>
-      <ul>
-        {notes.map(note => <OneNote key={note.id} note={note} />)}
-      </ul>
+      <h2>name: {country.name.common}</h2>
+      <p>area: {country.area}</p>
+      <p>language: {Object.keys(country.languages).map((key, index) => {
+        return (
+          <small>      { key }: {country.languages[key]} </small>
+        )
+      })}</p>
+      <p>flag: {country.flag}</p>
     </div>
   )
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [initCountries, setInitCountries] = useState([])
+  let promptWord = 'Please input more detail'
+  const hooks = () => {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then((response) => {
+        setCountries(response.data)
+        setInitCountries(response.data)
+      })
+  }
+
+  useEffect(hooks, [])
+
+  const handleFilterCountry = (event) => {
+    let inputCountry = event.target.value;
+    let filteredCountry = []
+    initCountries.map(country => {
+      if (country.name.common.includes(inputCountry) === true) {
+        filteredCountry.push(country)
+      }
+    })
+    if (filteredCountry.length > 1) {
+      promptWord = 'Please input more detail'
+    } else {
+      promptWord = ''
+    }
+    setCountries(filteredCountry)
+  }
+
+  if (countries.length === 1) {
+    return (
+      <div>
+        <form>
+          <input onChange={handleFilterCountry} placeholder='input filter country'></input>
+        </form>
+        <CountryDetail country={countries[0]} />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <form>
+          <small>{promptWord}:   </small>
+          <input onChange={handleFilterCountry} placeholder='input filter country'></input>
+        </form>
+        <ul>
+          {countries.map(country => <OneNote key={country.name.common} country={country} />)}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App
