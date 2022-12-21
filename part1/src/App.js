77a3d90eca09b00c17a92b316aeed6aa2d37ff19
zzsealy <1147575371@qq.com>
2022-12-21 15:38:26 +0800

    wip: practice 2.22

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,76 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
+import axios from 'axios'
 
-const Number = ({ person }) => {
+const OneNote = ({note}) => {
   return (
-    <li>{person.name}: {person.phone}</li>
+    <li>
+      <p>{note.name}: {note.number}</p>
+    </li>
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '88998899'}
-  ])
-  const [beforeFilterPerson, setBeforeFilterPerson] = useState('')
-  const [newName, setNewName] = useState('')
-  const [newPhone, setNewPhone] = useState('')
-
-  const handleNewName = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNewPhone = (event) => {
-    setNewPhone(event.target.value)
-  }
-
-
-  const handleFilterName = (event) => {
-    debugger;
-    let filterKey = event.target.value
-    let newPersons = []
-    if (filterKey) {
-      beforeFilterPerson.map(person => {
-        let name = person.name
-        if (name.search(filterKey) != -1) {
-          newPersons.push(person)
-        }
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const hooks = () => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons')
+      .then((response) => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
       })
-      setPersons(newPersons)
-    } else {
-      setPersons(beforeFilterPerson)
-    }
   }
-
-
-  const submitNewName = (event) => {
-    event.preventDefault()
-    let hasPerson = persons.filter((person) => person.name === newName)
-    if (hasPerson.length === 0) {
-      const newPerson = {
-        name: newName,
-        phone: newPhone
-      }
-      setPersons(persons.concat(newPerson))
-      setBeforeFilterPerson(persons.concat(newPerson))
-      setNewName('')
-      setNewPhone('')
-    } else {
-      alert(`${newName} is already added to phone book`)
-    }
-  }
+  useEffect(hooks, [])
+  console.log('render', notes.length, 'notes')
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <p>Filter the name with:  <input placeholder='input filter name' onChange={handleFilterName}></input> </p>
       <ul>
-        {persons.map(person => <Number key={person.name} person={person} />)}
-      </ul> 
-      <h2>Numbers</h2>
-      <form onSubmit={submitNewName}>
-        <input value={newName} placeholder='Please input a new name' onChange={handleNewName} />
-        <input value={newPhone} placeholder='Please input a new phone number' onChange={handleNewPhone} />
-        <button type="submit">add</button>
-      </form>
+        {notes.map(note => <OneNote key={note.id} note={note} />)}
+      </ul>
     </div>
   )
 }
