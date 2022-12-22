fddf2fc0936fe6b5d20d8eaab7127926f9e61b11
zzsealy <1147575371@qq.com>
2022-12-22 23:51:37 +0800

    wip: practice-2.15 新添加的电话簿 会发送请求到后台"

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,33 +1,59 @@
 import { useState, useEffect} from 'react'
 import axios from 'axios'
 
-const OneNote = ({note}) => {
+const OnePerson = ({person}) => {
   return (
     <li>
-      <p>{note.name}: {note.number}</p>
+      <p>{person.name}: {person.number}</p>
     </li>
   )
 }
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newPerson, setNewPerson] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const [showAll, setShowAll] = useState(true)
   const hooks = () => {
     console.log('effect')
     axios.get('http://localhost:3001/persons')
       .then((response) => {
         console.log('promise fulfilled')
-        setNotes(response.data)
+        setPersons(response.data)
       })
   }
   useEffect(hooks, [])
-  console.log('render', notes.length, 'notes')
+  console.log('render', persons.length, 'persons')
+
+  const handleAddPerson = event => {
+    setNewPerson(event.target.value)
+  }
+
+  const handleNewNumber = event => {
+    setNewNumber(event.target.value)
+  }
+
+  const handlerSubmitNewPerson = () => {
+    const url = 'http://localhost:3001/persons'
+    let newPersonObj = {
+      name: newPerson,
+      number: newNumber
+    }
+    axios.post(url, newPersonObj)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+      })
+  }
 
   return (
     <div>
+      <form>
+        <input value={newPerson} onChange={handleAddPerson} placeholder='input new person name'></input><br></br>
+        <input value={newNumber} onChange={handleNewNumber} placeholder='input new call number'></input><br></br>
+        <button onClick={handlerSubmitNewPerson}>submit</button>
+      </form>
       <ul>
-        {notes.map(note => <OneNote key={note.id} note={note} />)}
+        {persons.map(person => <OnePerson key={person.id} person={person} />)}
       </ul>
     </div>
   )
