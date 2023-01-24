c7ee756fa208d17b2fc49230a361aa2ad0564d9d
zzsealy <1147575371@qq.com>
2023-01-24 21:18:22 +0800

    wip: init express

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,41 @@
+const express = require('express')
 
-const http = require('http')
+const app = express()
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end('hello world')
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2022-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2022-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2022-05-30T19:20:14.298Z",
+      important: true
+    }
+  ]
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Service running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
-// console.log('hello world!')
