ae0411495eae9366fa556777ef415e608e3d02ad
zzsealy <1147575371@qq.com>
2023-02-01 18:01:41 +0800

    wip: 暂存

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,24 @@ app.get('/api/notes', (request, response) => {
 })
 
 
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.find(note => note.id !== id)
+  response.status(204)
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
