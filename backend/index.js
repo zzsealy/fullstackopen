b990f96e2596681f2af0a9ea633604b763953c12
zzsealy <1147575371@qq.com>
2023-02-01 21:59:32 +0800

    wip: 判断不存在content

--- backend/index.js
+++ backend/index.js
@@ -53,8 +53,29 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0
+  return maxId + 1
+}
+
+
 app.post('/api/notes', (request, response) => {
-  const note = request.body
+  const body = request.body
+  
+  if(!body.content) {
+    return response.status(400).json({
+      error: 'content error'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    data: new Date(),
+    id: generateId(),
+  }
+  
+  notes.concat(note)
   console.log(note)
   response.json(note)
 })
