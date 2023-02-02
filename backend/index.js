de24ccb0a4d9b2da4617ea30795bef78c35d571d
zzsealy <1147575371@qq.com>
2023-02-02 17:16:15 +0800

    wip: 错误捕获中间件

--- backend/index.js
+++ backend/index.js
@@ -58,10 +58,17 @@ app.get('/api/save_notes', (request, response) => {
 
 
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error)) // error会作为一个参数传递给下一个函数
 })
+
 
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -95,6 +102,17 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'id 传递错误'})
+  }
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
