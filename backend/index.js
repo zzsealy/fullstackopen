4cc77cac4139a64642dd83ec0a0fd9de4a32449a
zzsealy <1147575371@qq.com>
2023-02-02 16:45:12 +0800

    wip: montodb 增删改查

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
+const PORT = process.env.PORT
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-const mongoose = require('mongoose')
-
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const url = `mongodb://todo_admin:drq12345!@localhost:27017/todo`
-
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
+const Note = require('./models/note')
 
 
 let notes = [
@@ -51,6 +40,7 @@ app.get('/', (request, response) => {
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(
     notes => {
+    console.log(Date())
     response.json(notes)
   })
 })
@@ -68,13 +58,9 @@ app.get('/api/save_notes', (request, response) => {
 
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()  // 使用end来响应请求, 而不发送任何数据
-  }
+  })
 })
 
 
@@ -93,27 +79,23 @@ const generateId = () => {
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
-  
-  if(!body.content) {
-    return response.status(400).json({ // 这个return很重要 否则代码会执行到最后
-      error: 'content error'
-    })
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    data: new Date(),
-    id: generateId(),
-  }
-  
-  notes.concat(note)
-  console.log(note)
-  response.json(note)
+    date: new Date(),
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 
-const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
