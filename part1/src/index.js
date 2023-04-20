263f29905c4f5e2099c2dc8f0d117300b0a0b8e1
zzsealy <1147575371@qq.com>
2023-04-20 18:32:40 +0800

    wip: redux 暂存

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,44 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { createStore } from 'redux'
-
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
-}
-
-const store = createStore(counterReducer)
+import store from './reducers/noteReducer'
 
 const App = () => {
   return (
     <div>
-      <div>
-        {store.getState()}
-      </div>
-      <button
-        onClick={e => store.dispatch({ type: 'INCREMENT' })}
-      >
-        plus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'DECREMENT' })}
-      >
-        minus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'ZERO' })}
-      >
-        zero
-      </button>
+          {store.getState().map(note => 
+                <li key={note.id}>
+                  {note.content} <strong>{note.import ? 'important' : ''}</strong>
+                </li>
+            )}
     </div>
   )
 }
