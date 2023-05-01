2b1bc855f60fb9e0f83c6af6c56341e7d3987dc6
zzsealy <1147575371@qq.com>
2023-05-01 11:50:43 +0800

    wip: exercises 6.1

--- part6/src/App.js
+++ part6/src/App.js
@@ -1,5 +1,6 @@
 import Notes from "./components/Notes"
 import NewNote from "./components/NewNote"
+import Unicafe from "./unicafe"
 
 const App = () => {
 
@@ -7,6 +8,8 @@ const App = () => {
     <div>
       < NewNote />
       < Notes />
+      <h2>exercises 6.2, 6.2</h2>
+      <Unicafe />
     </div>
   )
 }
