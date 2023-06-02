f7040a895166d00180fe9257da9023d3d514115f
zzsealy <1147575371@qq.com>
2023-06-02 18:03:24 +0800

    wip:

--- part6/src/reducers/noteReducer.js
+++ part6/src/reducers/noteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import noteService from '../services/notes'
 
 const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0))
@@ -33,4 +34,11 @@ const noteSlice = createSlice({
 
 
 export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async dispatch => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
 export default noteSlice.reducer
